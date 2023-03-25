780bfc8762403b0d9360024347688f326aa62db8
puuuuuuuuuuuuups <128900737+puuuuuuuuuuuuups@users.noreply.github.com>
2023-03-25 13:55:26 +0200

    Add files via upload

---
+++ gyga.py
@@ -0,0 +1,8 @@
+from pygame import *
+windows = display.set_mode((700,500))
+display.set_caption("мырославчык програмыст")
+background = transform.scale(image.load(background.png),(700,500))
+
+game = True
+while game:
+    window
